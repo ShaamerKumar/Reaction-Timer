97efb30ad7a08cff7a29718effcaccf242cae721
Shaamer Kumar <shaamerkumar@berkeley.edu>
2022-07-19 15:12:15 +0800

    committing past work

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+  <h1> Shaamer's Reaction Timer</h1>
+  <p> Press play, then click the green button ASAP!</p>
+  <button @click="start" :disabled="isPlaying"> PLAY</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <Results v-if="showResult" :score="score"/>
+ </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {Block, Results},
+  data(){
+    return{
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResult:false
+    
+
+    }
+  },
+  methods: {
+    start() {
+      this.isPlaying = true
+      this.delay = 2000 + Math.random() * 3000
+      this.showResult = false
+
+    },
+    endGame(rctntm){
+      this.score = rctntm
+      this.isPlaying = false
+      this.showResult = true
+
+    }
   }
 }
 </script>
@@ -20,7 +45,15 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
+  
+}
+body{
+  background:lightgray;
+}
+button[disabled]{
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Reaction Time: {{score}} ms</p>
+  <h3 class="rank">{{rank}}</h3>
+  
+</template>
+
+<script>
+export default {
+    props:['score'],
+    data() {
+        return{
+            rank:null
+        }
+    },
+    mounted(){
+        if (this.score < 320){
+            this.rank = 'LEGENDARY REFLEXES'
+        }
+        else if (this.score < 450){
+            this.rank = 'Rapid Reflexes'
+        }
+        else {
+            this.rank = 'Wow you are slow...'
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
